b0eeb7ff2d3fec2400defaf0081edcc6b4ced707
ellen <13112060@bjtu.edu.cn>
2018-05-05 09:29:16 +0800

    5.5 1、所有的Conv2D由之前stride=2拆分成两个Conv2D，stride=1&&stride=2 2、考虑到年龄标签信息只作用在第一层（输入层），所以将Conv2D层（除第一 ）以外所有的都增加age、name、gender标签信息 下步： 需要测试Patch在新模型下面是否有用

--- discriminator.py
+++ discriminator.py
@@ -29,7 +29,8 @@ def discriminator_z_model(size_z, num_Dz_channels):
     return Model(inputs=inputs, outputs=current)
 
 # add the label of age + gender to input layer
-def discriminator_img_model(size_image, size_kernel, size_age_label, size_name_label, size_gender_label, num_input_channels, num_Dimg_channels, num_Dimg_fc_channels):
+def discriminator_img_model(
+        size_image, size_kernel, size_age_label, size_name_label, size_gender_label, num_input_channels, num_Dimg_channels, num_Dimg_fc_channels, GANs):
 
     kernel_initializer = initializers.truncated_normal(stddev=0.02)
     bias_initializer = initializers.constant(value=0.0)
@@ -38,23 +39,53 @@ def discriminator_img_model(size_image, size_kernel, size_age_label, size_name_l
     input_images = Input(shape=(size_image, size_image, num_input_channels))
     # the label of age + gender
     input_ages_conv = Input(shape=(1, 1, size_age_label)) # (1, 1, 10*tile_ratio)
+    input_names_conv = Input(shape=(1, 1, size_name_label))
+    input_genders_conv = Input(shape=(1, 1, size_gender_label))
     input_ages_conv_repeat = Lambda(duplicate_conv, output_shape=(size_image, size_image, size_age_label),
                                    arguments={'times': size_image})(input_ages_conv) #(128, 128, 10*tile_ratio)
-
-    input_names_conv = Input(shape=(1, 1, size_name_label))
     input_names_conv_repeat = Lambda(duplicate_conv, output_shape=(size_image, size_image, size_name_label),
                                      arguments={'times': size_image})(input_names_conv)
-    input_genders_conv = Input(shape=(1, 1, size_gender_label))
     input_genders_conv_repeat = Lambda(duplicate_conv, output_shape=(size_image, size_image, size_gender_label),
                                        arguments={'times': size_image})(input_genders_conv)
-
     # concatenate
     current = Concatenate(axis=-1)([input_images, input_ages_conv_repeat, input_names_conv_repeat, input_genders_conv_repeat])
     
     num_layers = len(num_Dimg_channels)
+    size_current = size_image
     # conv layers with stride 2
     for i in range(num_layers):
-        name = 'D_img_conv' + str(i)
+        # if i == 0:
+        #     strides = 1
+        # else:
+        #     strides = 2
+        #     size_image = int(size_image / 2)
+        #
+        # name = 'D_img_conv' + str(i)
+        # current = Conv2D(
+        #     filters=num_Dimg_channels[i],
+        #     kernel_size=(size_kernel, size_kernel),
+        #     strides=(strides, strides),
+        #     padding='same',
+        #     kernel_initializer=kernel_initializer,
+        #     bias_initializer=bias_initializer,
+        #     name=name)(current)
+
+        name = 'D_img_conv' + str(i) + str('_') + str(1)
+        current = Conv2D(
+            filters=num_Dimg_channels[i],
+            kernel_size=(size_kernel, size_kernel),
+            strides=(1, 1),
+            padding='same',
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            name=name)(current)
+        if i != 0:
+            current = Lambda(tf.contrib.layers.batch_norm, output_shape=(size_image, size_image, int(current.shape[3])),
+                             arguments={'decay':0.9, 'epsilon': 1e-5, 'scale':True})(current)
+        current = Lambda(lrelu, output_shape=(size_image, size_image, int(current.shape[3])))(current)
+
+        size_current = int(size_current / 2)
+        name = 'D_img_conv' + str(i) + str('_') + str(2)
         current = Conv2D(
             filters=num_Dimg_channels[i],
             kernel_size=(size_kernel, size_kernel),
@@ -63,23 +94,50 @@ def discriminator_img_model(size_image, size_kernel, size_age_label, size_name_l
             kernel_initializer=kernel_initializer,
             bias_initializer=bias_initializer,
             name=name)(current)
-        size_image = int(size_image / 2)
-
         if i != 0:
-            current = Lambda(tf.contrib.layers.batch_norm, output_shape=(size_image, size_image, int(current.shape[3])),
+            current = Lambda(tf.contrib.layers.batch_norm, output_shape=(size_current, size_current, int(current.shape[3])),
                              arguments={'decay':0.9, 'epsilon': 1e-5, 'scale':True})(current)
-        current = Lambda(lrelu, output_shape=(size_image, size_image, int(current.shape[3])))(current)
+        current = Lambda(lrelu, output_shape=(size_current, size_current, int(current.shape[3])))(current)
+        if i < num_layers - 1:
+            input_ages_conv_repeat = Lambda(duplicate_conv, output_shape=(size_current, size_current, size_age_label),
+                                            arguments={'times': size_current})(input_ages_conv)  # (128, 128, 10*tile_ratio)
+            input_names_conv_repeat = Lambda(duplicate_conv, output_shape=(size_current, size_current, size_name_label),
+                                             arguments={'times': size_current})(input_names_conv)
+            input_genders_conv_repeat = Lambda(duplicate_conv, output_shape=(size_current, size_current, size_gender_label),
+                                               arguments={'times': size_current})(input_genders_conv)
+            current = Concatenate(axis=-1)([current, input_ages_conv_repeat, input_names_conv_repeat, input_genders_conv_repeat])
 
-    name = 'D_img_conv_final'
-    current = Conv2D(
-        filters=1,
-        kernel_size=(size_kernel, size_kernel),
-        strides=(1, 1),
-        padding='same',
-        kernel_initializer=kernel_initializer,
-        bias_initializer=bias_initializer,
-        activation='sigmoid',
-        name=name)(current)
+    # Patch GAN_D
+    # name = 'D_img_conv_final'
+    # if GANs == 'LSGAN':
+    #     current = Conv2D(
+    #         filters=1,
+    #         kernel_size=(size_kernel, size_kernel),
+    #         strides=(1, 1),
+    #         padding='same',
+    #         kernel_initializer=kernel_initializer,
+    #         bias_initializer=bias_initializer,
+    #         name=name)(current)
+    # elif GANs == 'cGAN':
+    #     current = Conv2D(
+    #         filters=1,
+    #         kernel_size=(size_kernel, size_kernel),
+    #         strides=(1, 1),
+    #         padding='same',
+    #         kernel_initializer=kernel_initializer,
+    #         bias_initializer=bias_initializer,
+    #         activation='sigmoid',
+    #         name=name)(current)
+
+    name = 'D_img_conv_final_1'
+    current = Flatten()(current)
+    current = Dense(units=num_Dimg_fc_channels, name=name)(current)
+    current = Lambda(lrelu, output_shape=(num_Dimg_fc_channels, ))(current)
+    name = 'D_img_conv_final_2'
+    if GANs == 'LSGAN':
+        current = Dense(units=1, name=name)(current)
+    elif GANs == 'cGAN':
+        current = Dense(units=1, activation='sigmoid', name=name)(current)
 
     # output
     return Model(inputs=[input_images, input_ages_conv, input_names_conv, input_genders_conv], outputs=current)
