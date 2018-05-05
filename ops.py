320ca0a04bc44445f7408d5156ac67800f0efcef
ellen <13112060@bjtu.edu.cn>
2018-05-05 09:48:11 +0800

    5.5 1、更改sorted_compare_image_pairs，使得generate出图片对应一个图片对儿。 2、图片对儿包括与real_image仅age不同的图中，随机抽样一张作为real_image的图片对compare_image

--- ops.py
+++ ops.py
@@ -12,7 +12,7 @@ from pylab import *
 import matplotlib as mpl
 # mpl.use('Agg')
 
-def load_image(
+def load_sample(
         image_path,  # path of a image
         image_size,  # expected size of the image
         image_value_range=(-1, 1),  # expected pixel value range of the image
@@ -23,9 +23,32 @@ def load_image(
     else:
         image = imread(image_path).astype(np.float32)
         # image = np.array(Image.open(image_path).resize((image_size, image_size)))
+
     image = imresize(image, [image_size, image_size])
     image = image.astype(np.float32) * (image_value_range[-1] - image_value_range[0]) / 255.0 + image_value_range[0]
     return image
+
+def load_image(filenames, size_image, size_scale, num_input_channels):
+
+    # path_queue = tf.train.string_input_producer(image_path, shuffle=True)
+    # image_reader = tf.WholeFileReader()
+    # _, image_file = image_reader.read(path_queue)
+    images = []
+
+    for filename in filenames:
+        file_contents = tf.read_file(filename)
+        image = tf.image.decode_jpeg(file_contents)
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = image * 2 - 1
+
+        image = tf.image.random_flip_left_right(image)
+        image = tf.image.resize_images(image, [size_scale, size_scale], method=tf.image.ResizeMethod.BILINEAR)
+        offset = tf.cast(tf.floor(tf.random_uniform([2], 0, size_scale - size_image + 1)), dtype=tf.int32)
+        image = tf.image.crop_to_bounding_box(image, offset[0], offset[1], size_image, size_image)
+        image.set_shape([size_image, size_image, num_input_channels])
+        images.append(image)
+
+    return images
 
 def age_group_label(label):
     if 0 <= label <= 5:
@@ -91,9 +114,10 @@ def load_weights(save_dir, EGD):
     EGD = EGD.load_weights(str(save_dir) + '/EGD.h5', by_name=True)
     return EGD
 
-def save_weights(save_dir, GD_model, epoch, batch):
+def save_weights(save_dir, GD_model, content_model, epoch, batch):
     print("\n\tsaving trained model_e", epoch, " ...")
     GD_model.save(filepath=save_dir +"/GD_" + str(epoch) + 'b' + str(batch) + ".h5", overwrite=True)
+    content_model.save(filepath=save_dir + "/content_" + str(epoch) + 'b' + str(batch) + ".h5", overwrite=True)
     return "SUCCESS!"
 
 def save_image(images, size_image, image_value_range, num_input_channels, epoch, batch, mode, image_path):
@@ -126,12 +150,13 @@ def save_image(images, size_image, image_value_range, num_input_channels, epoch,
 
 
 
-def save_loss(save_dir, loss_Dimg, loss_D, loss_GD, loss_image, loss_all):
+def save_loss(save_dir, loss_Dimg, loss_D, loss_GD, loss_content, loss_image, loss_all):
 
 
     np.save(save_dir + '/loss_Dimg(all).npy', np.array(loss_D))
     np.save(save_dir+'/loss_Dimg.npy', np.array(loss_Dimg))
     np.save(save_dir + '/loss_GD.npy', np.array(loss_GD))
+    np.save(save_dir + '/loss_content.npy', np.array(loss_content))
     np.save(save_dir + '/loss_image.npy', np.array(loss_image))
     np.save(save_dir+'/loss_all.npy', np.array(loss_all))
 
@@ -158,11 +183,13 @@ def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
 
 def name_gender_label(file_name):
-    names = ['Michael_Cera', 'Julianna_Margulies', 'Treat_Williams', 'Peter_Jackson', 'Roseanne_Barr', 'William_Katt', 'Sam_Raimi', 'Rosanna_Arquette', 'Tilda_Swinton', 'Tim_Daly', 'Dana_Delany', 'Jennifer_Tilly', 'Kevin_Pollak', 'Madonna', 'Tom_Sizemore', 'John_Goodman', 'Vicki_Lewis', 'Virginia_Madsen', 'Woody_Harrelson', 'Zeljko_Ivanek', 'Adam_Arkin', 'Alfre_Woodard', 'Jon_Bon_Jovi', 'Julia_Louis-Dreyfus', 'ulianna_Margulies', 'Julianne_Moore', 'Ray_Romano', 'Peter_Jackson', 'Matt_LeBlanc', 'Fran_Drescher', 'Hugh_Grant', 'Geena_Davis', 'Eric_Roberts', 'Kevin_Spacey', 'Jeff_Perry', 'Jennifer_Coolidge', 'Dylan_McDermott', 'Patricia_Clarkson', 'Jennifer_Tilly', 'Mykelti_Williamson', 'Michael_Cerveris', 'Melanie_Griffith', 'Kelsey_Grammer', 'Lauren_Graham', 'Mark_Wahlberg', 'Megyn_Price', 'Nathan_Fillion', 'Paget_Brewster', 'Paul_Rudd', 'Virginia_Madsen', 'Sean_Patrick_Flanery', 'Shannen_Doherty', 'Taraji_P._Henson', 'Tim_Guinee', 'Rachel_Weisz', 'Stephen_Baldwin', 'Mark_Ruffalo', 'Tina_Fey', 'Aaron_Eckhart', 'Adewale_Akinnuoye-Agbaje', 'Sharlto_Copley', 'Amanda_Peet', 'Bradley_Cooper', 'Carla_Gallo', 'Casper_Van_Dien', 'Christina_Hendricks', 'Maya_Rudolph', 'Martin_Freeman', 'Naomi_Watts', 'Denise_Richards', 'Edward_Norton', 'Elizabeth_Mitchell', 'Michael_Pitt', 'Emily_Procter', 'Lucy_Liu', 'Aaron_Johnson', 'Britt_Robertson', 'Caitlin_Stasey', 'Chris_Colfer', 'Diego_Boneta', 'Emma_Watson', 'Jennifer_Lawrence', 'Liam_Hemsworth', 'Rupert_Grint', 'Rose_McIver', 'Naya_Rivera', 'Daniel_Radcliffe', 'Amanda_Bynes', 'Brant_Daugherty', 'Dakota_Johnson', 'Hunter_Parrish', 'Emma_Stone', 'Kristen_Connolly', 'Keira_Knightley', 'Taylor_Kinney', 'Hayden_Christensen', 'Katy_Perry', 'January_Jones', 'Chace_Crawford', 'Lily_Rabe', 'Matt_Bomer', 'Masiela_Lusha', 'Matthew_Goode', 'Nick_Cannon', 'Britney_Spears', 'Aimee_Teegarden', 'Jesse_Tyler_Ferguson', 'Drea_de_Matteo', 'Corey_Haim', 'Shawn_Ashmore', 'Sarah_Drew', 'Emily_Deschanel', 'Hugh_Dancy', 'Lauren_German', 'Lee_Pace', 'Kate_Hudson', 'Luke_Wilson', 'Sarah_Michelle_Gellar', 'Patrick_Dempsey', 'Ryan_Kwanten', 'Ali_Larter', 'Desmond_Harrington', 'Milla_Jovovich', 'Samantha_Barks', 'Chelsea_Kane', 'Tom_Mison', 'Matthew_Goode', 'Clark_Duke', 'Bryce_Dallas_Howard', 'Rosario_Dawson', 'Eric_Dane', 'Deborah_Ann_Woll', 'Luke_Evans', 'Jason_Segel', 'Elizabeth_Mitchell']
+    names = ['Kurt_Russell', 'Olivia_Hussey', 'Robin_Williams', 'Mark_Hamill', 'Jane_Seymour', 'Lynda_Carter', 'Michael_Cera', 'Julianna_Margulies', 'Treat_Williams', 'Peter_Jackson', 'Roseanne_Barr', 'William_Katt', 'Sam_Raimi', 'Rosanna_Arquette', 'Tilda_Swinton', 'Tim_Daly', 'Dana_Delany', 'Jennifer_Tilly', 'Kevin_Pollak', 'Madonna', 'Tom_Sizemore', 'John_Goodman', 'Vicki_Lewis', 'Virginia_Madsen', 'Woody_Harrelson', 'Zeljko_Ivanek', 'Adam_Arkin', 'Alfre_Woodard', 'Jon_Bon_Jovi', 'Julia_Louis-Dreyfus', 'ulianna_Margulies', 'Julianne_Moore', 'Ray_Romano', 'Peter_Jackson', 'Matt_LeBlanc', 'Fran_Drescher', 'Hugh_Grant', 'Geena_Davis', 'Eric_Roberts', 'Kevin_Spacey', 'Jeff_Perry', 'Jennifer_Coolidge', 'Dylan_McDermott', 'Patricia_Clarkson', 'Jennifer_Tilly', 'Mykelti_Williamson', 'Michael_Cerveris', 'Melanie_Griffith', 'Kelsey_Grammer', 'Lauren_Graham', 'Mark_Wahlberg', 'Megyn_Price', 'Nathan_Fillion', 'Paget_Brewster', 'Paul_Rudd', 'Virginia_Madsen', 'Sean_Patrick_Flanery', 'Shannen_Doherty', 'Taraji_P._Henson', 'Tim_Guinee', 'Rachel_Weisz', 'Stephen_Baldwin', 'Mark_Ruffalo', 'Tina_Fey', 'Aaron_Eckhart', 'Adewale_Akinnuoye-Agbaje', 'Sharlto_Copley', 'Amanda_Peet', 'Bradley_Cooper', 'Carla_Gallo', 'Casper_Van_Dien', 'Christina_Hendricks', 'Maya_Rudolph', 'Martin_Freeman', 'Naomi_Watts', 'Denise_Richards', 'Edward_Norton', 'Elizabeth_Mitchell', 'Michael_Pitt', 'Emily_Procter', 'Lucy_Liu', 'Aaron_Johnson', 'Britt_Robertson', 'Caitlin_Stasey', 'Chris_Colfer', 'Diego_Boneta', 'Emma_Watson', 'Jennifer_Lawrence', 'Liam_Hemsworth', 'Rupert_Grint', 'Rose_McIver', 'Naya_Rivera', 'Daniel_Radcliffe', 'Amanda_Bynes', 'Brant_Daugherty', 'Dakota_Johnson', 'Hunter_Parrish', 'Emma_Stone', 'Kristen_Connolly', 'Keira_Knightley', 'Taylor_Kinney', 'Hayden_Christensen', 'Katy_Perry', 'January_Jones', 'Chace_Crawford', 'Lily_Rabe', 'Matt_Bomer', 'Masiela_Lusha', 'Matthew_Goode', 'Nick_Cannon', 'Britney_Spears', 'Aimee_Teegarden', 'Jesse_Tyler_Ferguson', 'Drea_de_Matteo', 'Corey_Haim', 'Shawn_Ashmore', 'Sarah_Drew', 'Emily_Deschanel', 'Hugh_Dancy', 'Lauren_German', 'Lee_Pace', 'Kate_Hudson', 'Luke_Wilson', 'Sarah_Michelle_Gellar', 'Patrick_Dempsey', 'Ryan_Kwanten', 'Ali_Larter', 'Desmond_Harrington', 'Milla_Jovovich', 'Samantha_Barks', 'Chelsea_Kane', 'Tom_Mison', 'Matthew_Goode', 'Clark_Duke', 'Bryce_Dallas_Howard', 'Rosario_Dawson', 'Eric_Dane', 'Deborah_Ann_Woll', 'Luke_Evans', 'Jason_Segel', 'Elizabeth_Mitchell']
 
     # woman: 0 man:1
-    genders = ['1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '1', '1', '0', '0', '1', '1', '1', '0', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '1', '0', '1', '1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '1', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '1', '1', '1', '0', '0', '1', '0', '1', '1', '0']
+    genders = ['1', '0', '1', '1', '0', '1', '1', '0', '1', '1', '0', '1', '1', '0', '0', '1', '0', '0', '1', '0', '1', '1', '0', '0', '1', '1', '1', '0', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '1', '0', '1', '1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '1', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '1', '0', '0', '1', '0', '1', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '1', '1', '1', '0', '0', '1', '0', '1', '1', '0']
 
+    # names = ['Kurt_Russell', 'Olivia_Hussey', 'Robin_Williams', 'Mark_Hamill', 'Jane_Seymour', 'Lynda_Carter']
+    # genders = ['1', '0', '1', '1', '0', '1']
     index = names.index(file_name)
     return index, int(genders[index])
 
@@ -294,28 +321,41 @@ def get_age_name_gender_list(file_names, size_batch, size_age, size_name, size_g
     age_list, name_list, gender_list = [], [], []
     for i, label in enumerate(file_names):
 
-        # temp = str(file_names[i]).split('/')[-1]
-        temp = str(file_names[i]).split('\\')[-1]
+        temp = str(file_names[i]).split('/')[-1]
+        # temp = str(file_names[i]).split('\\')[-1]
         age = int(temp.split('_')[0])
-        name = temp[temp.index('_') + 1: temp.index('00') - 1]
+        try:
+            name = temp[temp.index('_') + 1: temp.index('n00') - 1]
+        except ValueError:
+            name = temp[temp.index('_') + 1: temp.index('00') - 1]
         age = age_group_label(age)
         [name, gender] = name_gender_label(name)
 
-        age_list.append(age)
-        name_list.append(name)
-        gender_list.append(gender)
+        age_label = np.zeros((size_age))
+        age_label[age] = 1
+        name_label = np.zeros((size_name))
+        if int(size_name) > 1:
+            name_label[name] = 1
+        else:
+            name_label = name / size_name_total
+        gender_label = np.zeros((size_gender))
+        gender_label[gender] = 1
 
-    age_label = np.zeros((size_batch, size_age))
-    age_label[:, age] = 1
-    name_label = np.zeros((size_batch, size_name))
-    if int(size_name) > 1:
-        name_label[:, name] = 1
-    else:
-        name_label[:] = name / size_name_total
-    gender_label = np.zeros((size_batch, size_gender))
-    gender_label[:, gender] = 1
+        age_list.append(age_label)
+        name_list.append(name_label)
+        gender_list.append(gender_label)
 
-    return [age_label, name_label, gender_label]
+    # age_label = np.zeros((size_batch, size_age))
+    # age_label[:, age] = 1
+    # name_label = np.zeros((size_batch, size_name))
+    # if int(size_name) > 1:
+    #     name_label[:, name] = 1
+    # else:
+    #     name_label[:] = name / size_name_total
+    # gender_label = np.zeros((size_batch, size_gender))
+    # gender_label[:, gender] = 1
+
+    return [np.array(age_list), np.array(name_list), np.array(gender_list)]
 
 def draw_loss_metric(save_dir, npy_name):
 
@@ -411,9 +451,81 @@ def draw_loss_metric(save_dir, npy_name):
 
     # show()
 
+def preprocess_image(images, size_image, num_input_channels, size_scale):
+    # from keras import backend as K
+
+    inputs = keras.backend.placeholder(shape=(None, size_image, size_image, num_input_channels))
+    seed = np.random.randint(0, 2 ** 31 - 1)
+    inputs = tf.image.resize_images(inputs, [size_scale, size_scale], method=tf.image.ResizeMethod.AREA)
+    offset = tf.cast(tf.floor(tf.random_uniform([2], 0, size_scale - size_image + 1, seed=seed)),
+                     dtype=tf.int32)
+    inputs = tf.image.crop_to_bounding_box(inputs, offset[0], offset[1], size_image, size_image)
+
+    with tf.Session() as sess:
+        images = sess.run(inputs, feed_dict={inputs: images})
+
+    return images
+
+def get_intermediate_output(model, layer_name, inputs):
+    from keras.models import Model
+    intermediate_layer_model = Model(input=model.inputs, output=model.get_layer(name=layer_name).output)
+    intermediate_output = intermediate_layer_model.predict(inputs)
+    return intermediate_output[0]
+
+def image_resize_from_array(array_expand_dim, image_resize):
+    from keras.preprocessing import image as kimage
+    image_list = []
+    (num, height, width, dim) = array_expand_dim.shape
+    for i in range(num):
+        array = array_expand_dim[i]
+        array = (array + 1)/2 + 255.0
+        image = kimage.array_to_img(array)
+        image = imresize(image, [image_resize, image_resize])
+        image = image.astype(np.float32) * 2 / 255.0 - 1
+        image_list.append(image)
+    return np.array(image_list)
+
+def sorted_compare_image_pairs(real_images, file_names, size_age, size_name, size_gender, size_name_total):
+    compare_images_pick, compare_ages_pick, real_images_pick = [], [], []
+
+    [real_ages, real_names, real_genders] = get_age_name_gender_list(file_names, len(file_names), size_age, size_name, size_gender, size_name_total)
 
 
+    for i in range(len(real_ages)):
+        real_age, real_name, real_gender = real_ages[i], real_names[i], real_genders[i]
+        compare_images, compare_ages = [], []
+        for j in range(len(real_ages)):
+            if recover_age(real_ages[j]) != recover_age(real_age) and recover_name(real_names[j]) == recover_name(real_name):
+                compare_images.append(real_images[j])
+                compare_ages.append(real_ages[j])
+
+        count = len(compare_images)
+        if count > 0:
+            random_index = np.random.randint(0, count, 1)
+            print('--------------------randim index----------------' + str(random_index[0]))
+            compare_images_pick.append(compare_images[random_index[0]])
+            compare_ages_pick.append(compare_ages[random_index[0]])
+            real_images_pick.append(real_images[i])
+
+    return [np.array(real_images_pick), np.array(compare_images_pick), np.array(real_ages), np.array(compare_ages_pick), np.array(real_names), np.array(real_genders)]
+
+def recover_age(age_label):
+    for i in range(len(age_label)):
+        if int(age_label[i]) == 1:
+            return i
+
+def recover_name(name_label):
+    if len(name_label) == 1:
+        return name_label[0]
+    elif len(name_label) > 1:
+        for i in range(len(name_label)):
+            if int(name_label[i]) == 1:
+                return i
 
 if __name__ == '__main__':
-    image = imread('D:\\workspace\\tensorflow\\FA-Unet-GANs\\data\\128_sample\\24_Michael_Cera_0004.jpg').astype(np.float32)
-    save_image(image, 128, (-1, 1), 3, 0, 0, None, 'D:/')
+    from glob import glob
+
+    file_names = glob(os.path.join('./data/new_128', '*.jpg'))
+    # real_images = [load_sample(file_name, 128, image_value_range=(-1, 1), is_gray=False) for file_name in file_names]
+    # sorted_compare_image_pairs(real_images, file_names, 10, 6, 2, 6)
+    load_image(file_names, 128, 158, 3)
