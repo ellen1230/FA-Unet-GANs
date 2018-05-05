addd49c88d86dc128882ca60ae12b5e6ab26de16
ellen <13112060@bjtu.edu.cn>
2018-05-05 09:44:39 +0800

    5.5 更改loss_image的计算方法。 1、输入real_images、compare_images，计算两个tensor的l1距离 2、各种距离（loss_image距离，loss_content距离等是否归一化需要看实验结果）的尝试。

--- loss.py
+++ loss.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Lambda, Concatenate
+from keras.layers import Input, Lambda, Concatenate, Flatten
 from keras.models import Model
 from keras.layers.merge import Subtract, subtract
 import tensorflow as K
@@ -54,62 +54,63 @@ def loss_all_model(size_image, size_age_label, size_name_label, size_gender_labe
         outputs=[loss_D_fake, loss_D_real, loss_GD, loss_image])
 
 
-def loss_seperate_model(size_image, size_age_label, size_name_label, size_gender_label, num_input_channels, G, D, GANs):
-    input_real_images = Input(shape=(size_image, size_image, num_input_channels))
+def loss_seperate_model(size_batch, size_image, size_age_label, size_name_label, size_gender_label, num_input_channels, num_content_channels_pick,
+                        G, D, content, GANs):
+    input_compare_images = Input(shape=(size_image, size_image, num_input_channels))
     input_fake_images = Input(shape=(size_image, size_image, num_input_channels))
-    input_ages_conv = Input(shape=(1, 1, size_age_label))
+
+    inpute_compare_age_conv = Input(shape=(1, 1, size_age_label))
     input_names_conv = Input(shape=(1, 1, size_name_label))
     input_genders_conv = Input(shape=(1, 1, size_gender_label))
 
     # loss_GD
     if GANs == 'cGAN':
-        output_GD = D([input_fake_images, input_ages_conv, input_names_conv, input_genders_conv])
+        output_GD = D([input_fake_images, inpute_compare_age_conv, input_names_conv, input_genders_conv])
         loss_GD = Lambda(get_loss_sigmoid_cross_entropy, arguments={'labels': K.ones_like(output_GD)}, output_shape=(None,))(output_GD)
     elif GANs == 'LSGAN':
-        output_GD = D([input_fake_images, input_ages_conv, input_names_conv, input_genders_conv])
+        output_GD = D([input_fake_images, inpute_compare_age_conv, input_names_conv, input_genders_conv])
         output_GD_compare = Lambda(K.ones_like)(output_GD)
-        loss_GD = Lambda(get_loss_l2, output_shape=(None,),
-               arguments={'size_image': size_image, 'num_input_channels': num_input_channels})(Subtract()([output_GD, output_GD_compare]))
+        loss_GD = Lambda(mse_loss_LSGAN, output_shape=(None,),
+               arguments={'size_batch': size_batch})(Subtract()([output_GD, output_GD_compare]))
 
 
     # loss_image
-    output_image = input_real_images
-    output_re_image = G([input_real_images, input_ages_conv, input_names_conv, input_genders_conv])
-    output_image_value = Subtract()([output_image, output_re_image])
-    loss_image = Lambda(get_loss_l1, output_shape=(None,), arguments={'size_image': size_image, 'num_input_channels': num_input_channels})(output_image_value)
+    output_image_value = Subtract()([input_compare_images, input_fake_images])
+    loss_image = Lambda(get_loss_l1, output_shape=(None,), arguments={'size_image': 1, 'num_input_channels': 1})(output_image_value)
+
+    # loss_content
+    real_features = content(input_compare_images)
+    fake_features = content(input_fake_images)
+    loss_content = Lambda(get_loss_content, output_shape=(None,),
+                          arguments={'fake_features': fake_features, 'num_content_channels_pick': num_content_channels_pick})(real_features)
 
     return Model(
-        inputs=[input_real_images, input_fake_images, input_ages_conv, input_names_conv, input_genders_conv],
-        outputs=[loss_GD, loss_image])
+        inputs=[input_compare_images, input_fake_images, inpute_compare_age_conv, input_names_conv, input_genders_conv],
+        outputs=[loss_GD, loss_content, loss_image])
 
 
 
 
 def get_loss_l2(t, size_image, num_input_channels):
-    if int(num_input_channels) == 1:
-        return K.sqrt(K.reduce_sum(K.square(t)))
-    else:
-        return K.sqrt(K.reduce_sum(K.square(t)))
+    return K.sqrt(K.reduce_sum(K.square(t)))/size_image/size_image/num_input_channels
 
 def get_loss_l1(t, size_image, num_input_channels):
-    if int(num_input_channels) == 1:
-        return K.reduce_mean(K.abs(t))
-    else:
-        return K.reduce_mean(K.abs(t))
-
-def get_loss_D_real(x):
-    K.reduce_mean(K.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=K.ones_like(x)))
-    import tensorflow
-
-
-def get_loss_D_fake(x):
-    K.reduce_mean(K.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=K.zeros_like(x)))
-
-def get_loss_binary(x, binary):
-    from keras.losses import binary_crossentropy
-    return binary_crossentropy(x, binary)
+    return K.reduce_mean(K.abs(t))/size_image/size_image/num_input_channels
 
 def get_loss_sigmoid_cross_entropy(x, labels):
     return K.reduce_mean(K.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=labels))
+
+def get_loss_content(real_features, fake_features, num_content_channels_pick):
+    loss = 0
+    for i in range(len(num_content_channels_pick)):
+        (height, width, channels) = num_content_channels_pick[i]
+        loss = loss + K.sqrt(K.reduce_sum(K.square(real_features[i] - fake_features[i])))/height/width
+        # loss = loss + K.sqrt(K.reduce_sum(K.square(real_features[i] - fake_features[i])))
+    return loss
+
+
+def mse_loss_LSGAN(t, size_batch):
+    loss_val = K.reduce_mean(K.sqrt(2 * K.nn.l2_loss(t)) / size_batch)
+    return loss_val
 
 
